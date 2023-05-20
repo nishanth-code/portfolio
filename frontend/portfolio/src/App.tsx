5f8007c6d9f6ab5752fc7ca21706ab426be12976
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2023-05-20 11:02:32 +0000

    Update 3 files

--- frontend/portfolio/src/App.tsx
+++ frontend/portfolio/src/App.tsx
@@ -1,16 +1,17 @@
 
-import './App.css'
+import './App.scss'
 import About from './components/about.tsx'
+import Navbar from './components/navbar.tsx'
 
 function App() {
   
 
   return (
-    <>
+    <div className="app">
      
       <About />
-    </>
+    </div>
   );
 };
 
-export default App
+export default App;
---
+++ frontend/portfolio/src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar:React.FC = ()=>{
+    return(
+       <nav>
+           <div>
+               <img >
+            </div>
+      </nav>
+    );
+};
+export default Navbar;
